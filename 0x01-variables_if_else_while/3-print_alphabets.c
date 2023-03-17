030daee83ad855c4d87934cd7507444b249060d5
TapiwaB <tapiwasetimela@gmail.com>
2023-03-17 10:55:39 -0700

    alphabets 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : 'checks if number is zero,negative or positive;
+ * Return: always 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n = 97;
+	int a = 65;
+
+	while (n <= 122)
+		putchar (n);
+	while (a <= 98)
+		putchar (a);
+	putchar('\n');
+	return (0);
+}
