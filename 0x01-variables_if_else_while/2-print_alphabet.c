b12c97fb813ac9e23f74ad672367637ff54d5402
TapiwaB <tapiwasetimela@gmail.com>
2023-03-17 10:27:21 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : 'checks if number is zero,negative or positive;
+ * Return: always 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'a'; i++)
+		putchar (i);
+	putchar ("\n");
+	return (0);
+}
