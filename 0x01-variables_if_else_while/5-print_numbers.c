b682eb5663fe37b44126879f8e675bc46c8dfd00
TapiwaB <tapiwasetimela@gmail.com>
2023-03-19 03:50:50 -0700

    print numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : 'checks if number is zero,negative or positive;
+ * Return: always 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+		for (n = 0; n <= 9; n++)
+		{
+			printf("%d", n);
+		}
+		printf("\n");
+	return (0);
+}
