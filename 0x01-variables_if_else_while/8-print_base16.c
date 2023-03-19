721e41381290f81d258c44ebaf39a84a45a664d1
TapiwaB <tapiwasetimela@gmail.com>
2023-03-19 06:14:18 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : 'checks if number is zero,negative or positive;
+ * Return: always 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar (n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar (m);
+	}
+	putchar ('\n');
+	return (0);
+}
