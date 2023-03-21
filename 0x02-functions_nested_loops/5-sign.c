28ae59190c4c639d209df13fd7ad722cfc322a2a
TapiwaB <tapiwasetimela@gmail.com>
2023-03-21 12:20:02 -0700

    alphabets and signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - prints the sign 
+ * The number to be checked
+ * Return: Return 1 for positive num, -1 for negative num or 0 for other.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
